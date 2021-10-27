f06577902fd48587cdf45db8aa3f5875b2e1bcde
iaspnetcore <290996724@qq.com>
2021-10-27 11:21:31 +0800

    step 4: Add Configure options  from appsettings.json

--- src/Miniblog.Core/Program.cs
+++ src/Miniblog.Core/Program.cs
@@ -6,15 +6,21 @@ builder.Services.AddControllersWithViews();
 // Add custom services to the container.
 builder.Services.AddSingleton<IUserServices, BlogUserServices>();
 builder.Services.AddSingleton<IBlogService, FileBlogService>();
-builder.Services.Configure<BlogSettings>(this.Configuration.GetSection("blog"));
+
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddMetaWeblog<MetaWeblogService>();
 
+//Add  Configure
+//https://github.com/dotnet/AspNetCore.Docs/blob/main/aspnetcore/fundamentals/configuration/index/samples/6.x/ConfigSample/Program.cs
+builder.Configuration.AddJsonFile("appsettings.json");
 
+builder.Services.Configure<BlogSettings>(
+    builder.Configuration.GetSection("blog"));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
