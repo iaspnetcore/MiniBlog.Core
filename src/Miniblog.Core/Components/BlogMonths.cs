52bc2c233fbf70964b1dddc9af03509d04764ee8
iaspnetcore <290996724@qq.com>
2022-01-28 07:40:44 +0800

    Creating a view component

---
+++ src/Miniblog.Core/Components/BlogMonths.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace Miniblog.Core.Components
+{
+    public class BlogMonthsViewComponent : ViewComponent
+    {
+       
+
+        public BlogMonthsViewComponent()
+        {
+         
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(
+        int maxPriority, bool isDone)
+        {
+            
+            return View();
+        }
+        
+    }
+}
